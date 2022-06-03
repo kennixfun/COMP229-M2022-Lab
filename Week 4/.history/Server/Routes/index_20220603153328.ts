25722287c578278bfa67a77efa8c6655a4b5c9a0
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-06-03 15:36:42 -0400

    added basic routes in Router file

--- Week 4/.history/Server/Routes/index_20220603153328.ts
+++ Week 4/.history/Server/Routes/index_20220603153328.ts
@@ -16,19 +16,9 @@ router.get('/about', function(req: express.Request, res: express.Response, next:
   res.render('index', { title: 'About Us' , page: 'about'});
 });
 
-/* GET projects page. */
-router.get('/projects', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Our Projects' , page: 'projects'});
-});
-
-/* GET services page. */
-router.get('/services', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Our Services' , page: 'services'});
-});
-
-/* GET contact page. */
-router.get('/contact', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Contact Us' , page: 'contact'});
+/* GET home page. */
+router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Home' , page: 'home'});
 });
 
 export  default router;
