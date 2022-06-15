96f59e1334112aa1e77c5df4f68a93bfbc7d5b8f
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-06-15 16:01:14 -0400

    completed auth functions

--- Week 6/.history/Server/Controllers/auth_20220615155609.ts
+++ Week 6/.history/Server/Controllers/auth_20220615155609.ts
@@ -76,20 +76,10 @@ export function ProcessRegisterPage(req: express.Request, res: express.Response,
             }
             return res.redirect('/register');
         }
-
-        // everything is ok - user has been registered
-
-        // automatically login the user
-        return passport.authenticate('local')(req, res, function()
-        {
-            return res.redirect('/movie-list');
-        })
     });
 }
 
 export function ProcessLogoutPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    req.logOut();
-
-    res.redirect('/login');
+    
 }
--- Week 6/.history/Server/Controllers/auth_20220615155641.ts
+++ Week 6/.history/Server/Controllers/auth_20220615155641.ts
@@ -80,16 +80,11 @@ export function ProcessRegisterPage(req: express.Request, res: express.Response,
         // everything is ok - user has been registered
 
         // automatically login the user
-        return passport.authenticate('local')(req, res, function()
-        {
-            return res.redirect('/movie-list');
-        })
+        
     });
 }
 
 export function ProcessLogoutPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    req.logOut();
-
-    res.redirect('/login');
+    
 }
