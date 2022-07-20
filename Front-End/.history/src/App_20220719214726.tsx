9926bc414b1a683ba7e6fbc2c296fbaca4f8ec33
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-07-19 21:52:26 -0400

    added about page

--- Front-End/.history/src/App_20220719214726.tsx
+++ Front-End/.history/src/App_20220719214726.tsx
@@ -25,7 +25,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
-          
         </Routes>
 
         <Footer></Footer>
