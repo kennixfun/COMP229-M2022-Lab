83ef2b3d53d345b72ee59f5eee418a6374cfe82a
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-07-19 21:43:15 -0400

    added footer component

--- Front-End/.history/src/App_20220719213837.tsx
+++ Front-End/.history/src/App_20220719213837.tsx
@@ -13,6 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
